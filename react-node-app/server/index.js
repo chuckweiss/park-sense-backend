ce71b75a20008d43c5c31e4a17f5c59de1347c92
Chuck <charlieweiss6@gmail.com>
2022-02-24 04:21:29 -0500

    submodule

--- react-node-app/server/index.js
+++ react-node-app/server/index.js
@@ -1,11 +1,22 @@
+const path = require("path");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const build_path = "../capstone-frontend/my-app/build";
+
+app.use(express.static(path.resolve(__dirname, build_path)));
+
+// Have Node serve the files for our built React app
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, build_path, "index.html"));
 });
 
 app.listen(PORT, () => {
